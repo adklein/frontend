9f28de14912c42cdeaaaf4e5e10028644b9a4b4e
Aaron Klein <adklein@umich.edu>
2016-02-19 16:57:38 -0800

    Added first piece of js code to include name and title on header

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -1,3 +1,27 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ //$("#main").append("Aaron Klein");
+ 
+ var SoL = 299792458;
+ var MtoC = 100;
+ var nanoSec = 1.0/1000000000;
+ var speed = SoL * MtoC * nanoSec;
+ 
+ //$("#main").append(speed);
+ 
+ var awesomeThoughts = "I am Aaron and I am AWESOME!";
+ 
+ console.log(awesomeThoughts);
+ 
+ var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+ 
+ //$("#main").append(funThoughts);
+ 
+ var formattedName = HTMLheaderName.replace("%data%","Aaron Klein");
+ 
+ var formattedRole = HTMLheaderRole.replace("%data%","Full-Stack Web Dev");
+ 
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
