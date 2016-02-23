685a24c6c5013655b8cf0ece11c5058cee0b1d5e
Aaron Klein <adklein@umich.edu>
2016-02-22 16:57:08 -0800

    cleaned up resumeBuilder to only have 4 main vars and blacked out incomplete sections on the resume website

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -4,18 +4,18 @@ This is empty on purpose! Your code to build the resume will go here.
  
  //$("#main").append("Aaron Klein");
  
- var SoL = 299792458;
- var MtoC = 100;
- var nanoSec = 1.0/1000000000;
- var speed = SoL * MtoC * nanoSec;
+// var SoL = 299792458;
+// var MtoC = 100;
+// var nanoSec = 1.0/1000000000;
+// var speed = SoL * MtoC * nanoSec;
  
  //$("#main").append(speed);
  
- var awesomeThoughts = "I am Aaron and I am AWESOME!";
+// var awesomeThoughts = "I am Aaron and I am AWESOME!";
  
- console.log(awesomeThoughts);
+// console.log(awesomeThoughts);
  
- var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+// var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
  
  //$("#main").append(funThoughts);
  
@@ -36,7 +36,7 @@ This is empty on purpose! Your code to build the resume will go here.
  
  var bio = {"name": "Aaron Klein", 
 	"role": "Full Stack Data Scientist", 
-	"contactinfo" : contactInfo, 
+	"contacts" : contactInfo, 
 	"picture" : pictureURL, 
 	"welcomeMessage" : welcome, 
 	"skills" : skills
@@ -44,20 +44,46 @@ This is empty on purpose! Your code to build the resume will go here.
  
  $("#main").append(bio.name);
  $("#main").append(bio.role);
- $("#main").append(bio.contactinfo.email);
+ $("#main").append(bio.contacts.email);
  
  var work = {};
- var education = {};
+ var education = {
+     "schools": [
+     {
+         "name": "University of Michigan",
+         "city": "Ann Arbor, MI, USA",
+         "degree": "BSc",
+         "major": ["Aerospace Engineering"]
+     },
+     {
+         "name": "Imperial College London",
+         "city": "London, UK",
+         "degree": "MSc",
+         "major": ["Aeronautical Engineering"]
+     }
+    ]
+ };
  
- education.name = "Imperial College London";
- education.Degree = "Masters of Science";
- education.Year = 2015;
- education.City = "London";
+ //education.name = "Imperial College London";
+ //education.Degree = "Masters of Science";
+ //education.Year = 2015;
+ //education.City = "London";
  
  work.position = "Analyst/Researcher";
  work.employer = "Klein Lawyers";
  work.Year = 2015;
  work.City = "Vancouver";
+ 
+ var projects = {
+     "project": [
+     {
+         "title": "First Project",
+         "date": "Jan ____ to Dec ____",
+         "description": "What was done",
+         "images": ["website.URL"]
+     }
+    ]
+ };
  
  $("#main").append(work.position);
  $("#main").append(education.name);
